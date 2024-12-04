69afd2d3c080c542401806848dbf91e496228bfe
Juan <j.villamizar@epilot.cloud>
2024-12-04 17:51:44 +0100

    Refactor logging in ZeroShotLabeler class

--- zero_shot_labeler/__init__.py
+++ zero_shot_labeler/__init__.py
@@ -32,40 +32,45 @@ class ZeroShotLabeler:
                 cls._instance = super(ZeroShotLabeler, cls).__new__(cls)
         return cls._instance
 
+    def log(self, *args, **kwargs):
+        print(f"[> {self.__class__.__name__}]:", *args, **kwargs)
+
     @classmethod
     def preload_model(cls, *, model: str = DEFAULT_MODEL, force_download: bool = False):
         """Preload the model during container initialization"""
         if MODEL_PATH.exists() and not force_download:
-            print(f"Model already exists at {MODEL_PATH}")
+            print(f"[{cls.__name__}]: Model already exists at {MODEL_PATH}")
             return
 
-        print(f"Preloading model from {model} to {MODEL_PATH}")
+        print(f"[{cls.__name__}]: Preloading model from {model} to {MODEL_PATH}")
         starting_time = time()
         snapshot_download(
             model,
             allow_patterns=["*.json", "*.safetensors", "*.model"],
+            cache_dir=MODEL_PATH,
             local_dir=MODEL_PATH,
         )
-        print(f"Model preloaded in {time() - starting_time:.2f} seconds")
+        print(
+            f"[{cls.__name__}]: Model preloaded in {time() - starting_time:.2f} seconds"
+        )
 
     def __init__(self, model: str = DEFAULT_MODEL):
-        if not hasattr(self, "pipeline"):
-            model_path = MODEL_PATH.as_posix() if MODEL_PATH.exists() else model
-            starting_time = time()
-            print(f"Loading model from {model_path}")
-            tokenizer = AutoTokenizer.from_pretrained(model)
-            self.pipeline = pipeline(
-                "zero-shot-classification",
-                tokenizer=tokenizer,
-                model=model_path,
-            )
-            print(f"Model loaded in {time() - starting_time:.2f} seconds")
+        model_path = MODEL_PATH.as_posix() if MODEL_PATH.exists() else model
+        starting_time = time()
+        self.log(f"Loading model from {model_path}")
+        tokenizer = AutoTokenizer.from_pretrained(model)
+        self.pipeline = pipeline(
+            "zero-shot-classification",
+            tokenizer=tokenizer,
+            model=model_path,
+        )
+        self.log(f"Model loaded in {time() - starting_time:.2f} seconds")
 
     def __call__(self, text: str, labels: list[str]) -> LabelerOutput:
         starting_time = time()
-        print(f"Classifying text: {text}")
+        self.log(f"Classifying text: {text}")
         output = cast(dict[str, list], self.pipeline(text, labels))
-        print(f"Classification in {time() - starting_time:.2f} seconds")
+        self.log(f"Classification in {time() - starting_time:.2f} seconds")
         labels = output.get("labels", [])
         scores = output.get("scores", [])
         return {label: score for label, score in zip(labels, scores)}
